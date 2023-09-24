ad33e666cf07b909c2cea2005a94be3d34986884
pritimunde1999 <pritimunde0109@gmail.com>
2023-09-24 13:49:08 +0530

    update with(.)

--- src/components/Child.js
+++ src/components/Child.js
@@ -11,7 +11,7 @@ const Child = ({showModal,setShowModal}) =>{
             showModal &&
             <div>
                 <h3>Modal Content</h3>
-                <p>This is the modal content</p>
+                <p>This is the modal content.</p>
             </div>
         }
        </div>
